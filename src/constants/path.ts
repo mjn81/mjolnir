702df66b18a5a9e4562dc026e25eb5600c2d0e26
mjn81 <mjjnn81@gmail.com>
2022-09-07 15:29:41 +0430

    [UPDATE] cors origin updated.

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,6 +1,9 @@
 export const UPLOAD_DIR = 'uploads';
 export const WHITE_LIST = [
   'http://localhost:3000',
+  'http://mjnLinux:3000',
   'http://localhost:3001',
+  'http://mjnLinux:3001',
   'https://mjolnir-front.vercel.app',
+  'https://muselib.vercel.app',
 ];

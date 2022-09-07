375ace367bc4463868beab863e424bb7adda85dc
mjn81 <mjjnn81@gmail.com>
2022-09-07 14:50:20 +0430

    [FIX] cors origins added.

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,1 +1,6 @@
 export const UPLOAD_DIR = 'uploads';
+export const WHITE_LIST = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://mjolnir-front.vercel.app',
+];
--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,6 +1,15 @@
 import cors from 'cors';
+import { WHITE_LIST } from 'constants/index';
+
 
 export const corsMiddleware = cors({
+  origin: (origin, callback) => { 
+    if (WHITE_LIST.indexOf(origin ?? '') !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   credentials: true,

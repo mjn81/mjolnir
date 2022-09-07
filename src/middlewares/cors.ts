859a199eaf12e77c10b1dd053fe594a534a144ca
mjn81 <mjjnn81@gmail.com>
2022-09-07 16:50:24 +0430

    [TEST] testing cors.

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,14 +1,10 @@
 import cors from 'cors';
-import { WHITE_LIST } from '../constants';
 
 export const corsMiddleware = cors({
-  origin: (origin, callback) => {
-    if (WHITE_LIST.indexOf(origin ?? '') !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   credentials: true,
